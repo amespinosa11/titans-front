6d038885d88df18955f608f46c622a2713775f9d
amespinosa <am.espinosa11@uniandes.edu.co>
2020-05-17 19:48:10 -0500

    Mostrar reportes

--- src/app/pages/resultados-prueba/resultados-prueba.component.ts
+++ src/app/pages/resultados-prueba/resultados-prueba.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EstrategiaService } from 'src/app/services/estrategia/estrategia.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-resultados-prueba',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadosPruebaComponent implements OnInit {
 
-  constructor() { }
+  logs = [];
+
+  constructor(private estrategiaService: EstrategiaService, private activatedRoute: ActivatedRoute,) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+    .subscribe( params  => {
+      this.estrategiaService.obtenerResultados(params['idEstrategia'])
+      .subscribe( (data:any) => {
+        console.log(data);
+        this.logs = data;
+      } )
+    });
+    
   }
 
 }
